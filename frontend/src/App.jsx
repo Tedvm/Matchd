d6d594a57c607b6ba4cd481ccb3930184ad86543
Tancrède AMIEL <amieltancrede@gmail.com>
2025-06-06 01:09:04 +0200

    upd

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,10 +6,7 @@ import Counter from './pages/Counter/Counter';
 import Users from './pages/Users/Users';
 import Navbar from './components/Navbar/Navbar';
 import SearchResults from './pages/SearchResults/SearchResults';
-<<<<<<< HEAD
 import MoviePage from './pages/Movies/MoviePage'; // adapte le chemin si tu changes
-=======
->>>>>>> 1ecf4a947633253a4de52726faa2a3f1272ea814
 import Login from './pages/Login/Login';
 import Account from './pages/Account/Account';
 
@@ -25,10 +22,7 @@ function App() {
         <Route path="users" element={<Users />} />
         <Route path="about" element={<About />} />
         <Route path="/search" element={<SearchResults />} />
-<<<<<<< HEAD
         <Route path="/movie/:id" element={<MoviePage />} />
-=======
->>>>>>> 1ecf4a947633253a4de52726faa2a3f1272ea814
         <Route path="login" element={<Login />} />
         <Route path="account" element={<Account />} />
       </Routes>
